4eb222a212bab24dff2feafc9978a00e7ede0857
madfr0st <suman.saurav117@gmail.com>
2020-01-07 13:37:58 +0530

    Updated

---
+++ src/Practice/D1096.java
@@ -0,0 +1,10 @@
+package Practice;
+
+import java.io.*;
+
+public class D1096 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+}
